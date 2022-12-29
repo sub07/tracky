47dc42e2d66215d44d62b5d2646328f4bd637127
sub07 <pardomarius@gmail.com>
2022-12-29 04:11:37 +0100

    cleanup

--- src/model/pattern.rs
+++ src/model/pattern.rs
@@ -1,6 +1,5 @@
 use std::slice::Iter;
 
-use crate::model::{Direction, PatternLineElement};
 use crate::model::pattern_line::PatternLine;
 
 pub struct Pattern {
@@ -22,14 +21,5 @@ impl Pattern {
 
     pub fn len(&self) -> usize {
         self.lines.len()
-    }
-
-    pub(super) fn move_overflows(&mut self, pattern_local_x: usize, cursor_y: usize, direction: &Direction) -> bool {
-        match direction {
-            Direction::Left => pattern_local_x == 0,
-            Direction::Right => pattern_local_x == PatternLineElement::LINE_LEN - 1,
-            Direction::Up => cursor_y == 0,
-            Direction::Down => cursor_y == self.lines.len() - 1,
-        }
     }
 }
--- src/model/pattern_line.rs
+++ src/model/pattern_line.rs
@@ -1,5 +1,4 @@
 use derive_new::new;
-use sdl2::sys::ssize_t;
 use crate::model::field::note::NoteField;
 
 use crate::model::field::velocity::VelocityField;
